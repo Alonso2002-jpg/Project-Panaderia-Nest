5805bc0a3e20d424796b06532e69711159c11403
kevinbermudes <39884727+kevinbermudes@users.noreply.github.com>
2024-01-27 23:39:30 +0100

    feat(module)the module is added

--- src/rest/personal/personal.module.ts
+++ src/rest/personal/personal.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
-import { PersonalService } from './personal.service';
-import { PersonalController } from './personal.controller';
+import {Module} from '@nestjs/common';
+import {PersonalService} from './personal.service';
+import {PersonalController} from './personal.controller';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {CacheModule} from '@nestjs/cache-manager'
+import {PersonalEntity} from "./entities/personal.entity";
+import {Category} from "../category/entities/category.entity";
+import {MapperPersonal} from "./mapper/mapperPersonal";
 
 @Module({
-  controllers: [PersonalController],
-  providers: [PersonalService],
+    imports: [
+        TypeOrmModule.forFeature([Category, PersonalEntity]),
+        CacheModule.register(),
+    ],
+
+    controllers: [PersonalController],
+    providers: [PersonalService, MapperPersonal],
 })
-export class PersonalModule {}
+export class PersonalModule {
+}
