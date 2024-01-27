9eb4a029bf366a76d4fa29fb250c733d139df822
MiguelZanottto <migzanotto18@hotmail.com>
2024-01-27 20:57:08 +0100

    Servicio de product

--- src/rest/product/product.service.ts
+++ src/rest/product/product.service.ts
@@ -1,10 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, Logger} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import {ProductMapper} from "./mappers/product-mapper";
+import {Product} from "./entities/product.entity";
+import {Repository} from "typeorm";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Category} from "../category/entities/category.entity";
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
+  private readonly logger = new Logger(ProductService.name)
+
+  constructor(
+      @InjectRepository(Product)
+      private readonly productRepository: Repository<Product>,
+      @InjectRepository(Category)
+      private readonly categoryRepository: Repository<Category>,
+      private readonly productMapper: ProductMapper) {
+  }
+  async create(createProductDto: CreateProductDto) {
+
     return 'This action adds a new product';
   }
 
