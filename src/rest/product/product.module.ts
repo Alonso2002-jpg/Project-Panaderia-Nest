d8347667a14a6b59f2b395a0b549691bb6ec71da
MiguelZanottto <migzanotto18@hotmail.com>
2024-01-28 23:12:58 +0100

    Creamos controlador de Product

--- src/rest/product/product.module.ts
+++ src/rest/product/product.module.ts
@@ -1,17 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
-import {ProductMapper} from "./mappers/product-mapper";
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {Product} from "./entities/product.entity";
-import {Category} from "../category/entities/category.entity";
+import { ProductMapper } from './mappers/product-mapper'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Product } from './entities/product.entity'
+import { Category } from '../category/entities/category.entity'
 import { CacheModule } from '@nestjs/cache-manager'
 
 @Module({
   imports: [
       TypeOrmModule.forFeature([Product]),
-      TypeOrmModule.forFeature([Category]),
-      CacheModule.register(),
+    TypeOrmModule.forFeature([Category]),
+    TypeOrmModule.forFeature([Provider]),
+    CacheModule.register(),
   ],
   controllers: [ProductController],
   providers: [ProductService, ProductMapper],
