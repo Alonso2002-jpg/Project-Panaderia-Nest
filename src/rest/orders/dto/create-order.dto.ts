0bd30691b25881b2883d8f75d08a1ab76cb43a56
Joselyn Obando <obandofernandez1@gmail.com>
2024-01-30 20:00:48 +0100

    Creando el swagger en create.dto

--- src/rest/orders/dto/create-order.dto.ts
+++ src/rest/orders/dto/create-order.dto.ts
@@ -28,16 +28,19 @@ export class AddressDto {
     @IsNotEmpty()
     city: string
 
+    @ApiProperty({ example: 'Leganes',description: 'The name of the province', maxLength: 100, required: true})
     @IsString()
     @MaxLength(100)
     @IsNotEmpty()
     province: string
 
+    @ApiProperty({ example: 'Spain',description: 'The name of the country', maxLength: 100, required: true})
     @IsString()
     @MaxLength(100)
     @IsNotEmpty()
     country: string
 
+    @ApiProperty({ example: '28911',description: 'The number of the postcode', maxLength: 100, required: true})
     @IsString()
     @MaxLength(100)
     @IsNotEmpty()
@@ -45,16 +48,19 @@ export class AddressDto {
 }
 
 export class ClientDto {
+    @ApiProperty({ example: 'Evelyn Vanessa Obando Fernandez',description: 'Fullname of you ', maxLength: 100, required: true})
     @IsString()
     @MaxLength(100)
     @IsNotEmpty()
     fullName: string
 
+    @ApiProperty({ example: 'obandofernandez1@gmail.com',description: 'Email of you', maxLength: 100, required: true})
     @IsString()
     @MaxLength(100)
     @IsEmail()
     email: string
 
+    @ApiProperty({ example: '602697979',description: 'Your phone number', maxLength: 100, required: true})
     @IsString()
     @MaxLength(100)
     @IsNotEmpty()
@@ -65,20 +71,24 @@ export class ClientDto {
 }
 
 export class OrderLineDto {
+    @ApiProperty({example: 'd69cf3db-b77d-4181-b3cd-5ca8107fb6a9', description: 'Product ID', maxLength: 100, required: true})
     @IsNumber()
     @IsNotEmpty()
-    idProduct: number
+    idProduct: string
 
+    @ApiProperty({example: 9.99, description: 'Price of the product', minLength: 0, required: true})
     @IsNumber()
     @IsNotEmpty()
     @Min(0, { message: 'The price must be greater than 0' })
     priceProduct: number
 
+    @ApiProperty({example: 50, description: 'Stock of the product', minLength: 1, required: true})
     @IsNumber()
     @IsNotEmpty()
     @Min(1, { message: 'The stock must be greater than 0' })
     stock: number
 
+    @ApiProperty({example: 9.99, description: 'Total of the product', minLength: 0, required: true})
     @IsNumber()
     @IsNotEmpty()
     @Min(0, { message: 'The stock must be greater than 0' })
