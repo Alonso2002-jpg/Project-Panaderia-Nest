47c4a81e0499b60764866989312213bc69610972
Laura Garrido Arredondo <anoobmedic@gmail.com>
2024-01-30 18:21:33 +0100

    Adding cache in a comment and adding logger inside the controller of providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config'
 import { PersonalModule } from './personal/personal.module';
 import { ProductModule } from './rest/product/product.module';
 import { OrdersModule } from './rest/orders/orders.module';
+import { ProvidersModule } from "./rest/providers/providers.module";
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { OrdersModule } from './rest/orders/orders.module';
     DatabaseModule,
     ProductModule,
     OrdersModule,
+    ProvidersModule
   ],
   controllers: [],
   providers: [],
