9752a481f2817cd5e3630c077c8e9ad34c623256
kevinbermudes <kevinber783@gmail.com>
2024-02-04 02:09:10 +0100

    docs(mapperPersonal) documentation is added

--- src/rest/personal/mapper/mapperPersonal.ts
+++ src/rest/personal/mapper/mapperPersonal.ts
@@ -6,25 +6,44 @@ import {ResponsePersonalDto} from '../dto/response-personal.dto'
 import {Injectable} from '@nestjs/common'
 import {Category} from "../../category/entities/category.entity";
 
+/**
+ * Service class for mapping between DTOs and the PersonalEntity.
+ */
 @Injectable()
 export class MapperPersonal {
+    /**
+     * Maps a CreatePersonalDto and a Category to a new PersonalEntity.
+     * @param createPersonalDto DTO containing the data to create a new personal.
+     * @param categoria The category entity to which the personal belongs.
+     * @returns {PersonalEntity} A new instance of PersonalEntity.
+     */
     toEntity(
         createPersonalDto: CreatePersonalDto,
         categoria: Category,
     ): PersonalEntity {
-        const personalEntity = plainToClass(PersonalEntity, createPersonalDto)
-        personalEntity.section = categoria
-        personalEntity.id = uuidv4()
-        return personalEntity
+        // Create a new PersonalEntity instance from the CreatePersonalDto.
+        const personalEntity = plainToClass(PersonalEntity, createPersonalDto);
+        // Assign the provided Category to the new entity.
+        personalEntity.section = categoria;
+        // Generate a new UUID for the personal entity.
+        personalEntity.id = uuidv4();
+        return personalEntity;
     }
 
+    /**
+     * Maps a PersonalEntity to a ResponsePersonalDto.
+     * @param personal The PersonalEntity to be mapped.
+     * @returns {ResponsePersonalDto} The mapped ResponsePersonalDto.
+     */
     toResponseDto(personal: PersonalEntity): ResponsePersonalDto {
-        const dto = plainToClass(ResponsePersonalDto, personal)
+        // Create a new ResponsePersonalDto instance from the PersonalEntity.
+        const dto = plainToClass(ResponsePersonalDto, personal);
+        // If the entity has an associated section, map the name to the DTO's section field.
         if (personal.section) {
-            dto.section = personal.section.nameCategory
+            dto.section = personal.section.nameCategory;
         } else {
-            dto.section = null
+            dto.section = null;
         }
-        return dto
+        return dto;
     }
 }
