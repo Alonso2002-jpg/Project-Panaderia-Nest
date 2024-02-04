fcc27fb22bdf08031d1456fe87a4f3bc7dd6a724
kevinbermudes <kevinber783@gmail.com>
2024-02-04 01:00:07 +0100

    feat(appModule) AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,22 @@
-import { Module } from '@nestjs/common'
-import { CategoryModule } from './rest/category/category.module'
-import { PersonalModule } from './personal/personal.module'
-import { DatabaseModule } from './config/database/database.module'
-import { ConfigModule } from '@nestjs/config'
-import { PersonalModule } from './personal/personal.module';
-import { ProductModule } from './rest/product/product.module';
+import {Module} from '@nestjs/common'
+import {CategoryModule} from './rest/category/category.module'
+import {DatabaseModule} from './config/database/database.module'
+import {ConfigModule} from '@nestjs/config'
+import {ProductModule} from './rest/product/product.module';
+import {PersonalModule} from "./rest/personal/personal.module";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-    }),
-    CategoryModule,
-    PersonalModule,
-    DatabaseModule,
-    ProductModule,
-  ],
-  controllers: [],
-  providers: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: '.env',
+        }),
+        CategoryModule,
+        PersonalModule,
+        DatabaseModule,
+        ProductModule,
+    ],
+    controllers: [],
+    providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
